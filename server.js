28202f626ab0c87012af0ca72838e5fb12284189
kethankoushik04 <kethankoushik04@example.com>
2025-12-03 17:13:05 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import { connectDB } from "./src/config/db.js";
+import eventRoutes from "./src/routes/event.routes.js";
+
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+
+// debug logger
+app.use((req, res, next) => {
+console.log(`${req.method} ${req.path} - headers:`, req.headers["content-type"]);
+next();
+});
+
+
+app.use("/api/events", eventRoutes);
+
+
+app.get("/", (req, res) => res.json({ message: "Event backend running" }));
+
+
+const port = process.env.PORT || 4000;
+connectDB()
+.then(() => app.listen(port, () => console.log(`Server running on http://localhost:${port}`)))
+.catch((err) => console.error("DB connection failed:", err));
